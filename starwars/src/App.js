534b4213859025927beaf70fb405be87dcd294e2
kiamco <kiamcoo@gmail.com>
2019-08-03 10:17:39 -0700

    added semantic ui and completed mvp it looks like crap though

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import Axios from 'axios';
 import './App.css';
+import StarCard from "./components/PeopleCard"
 
 const App = () => {
+  const [people, setPeople] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +12,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+  useEffect(() => {
+    Axios.get(`https://swapi.co/api/people/`)
+      .then(response => {
+        console.log(response.data.results);
+        setPeople(response.data.results);
+
+   
+      })
+      .catch(err => console.log(`App.js/ err: ${err}`))
+  }, [])
+
+
+
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <StarCard info={people}/>
     </div>
   );
 }
---
+++ starwars/src/components/infoCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card } from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
+function Info(props){
+    return(
+        <Card>
+            <Card.Content>
+                <Card.Header>Name: {props.starName}</Card.Header>
+                <Card.Meta>Height: {props.height}</Card.Meta>
+                <Card.Description>Mass: {props.mass}</Card.Description>
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default Info;
